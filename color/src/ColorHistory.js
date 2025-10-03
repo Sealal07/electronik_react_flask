371e99f819b3c6cedf4d42603adea07510029c65
rudak_a <alex.rud.seal@gmail.com>
2025-10-03 09:17:18 +0700

    coloristory

--- color/src/ColorHistory.js
+++ color/src/ColorHistory.js
@@ -1,8 +1,43 @@
 import React from 'react';
 
-function ColorHistory({history, onDelete}){
+function ColorHistory({ history, onDelete }) {
     return (
-        <div></div>
+        <div>
+            <h2>История цветов</h2>
+            {/* если история пуста */}
+            {history.length === 0 ? (
+                <p>История цветов пуста</p>
+            ) : (
+                <div style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'center',
+                gap: '15px' }}>
+                    {history.map((color, index) => (
+                        <div key={color + index}
+                        style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                         justifyContent: 'center'}}>
+                            <div style={{
+                                    backgroundColor: color,
+                                    width: '100px',
+                                    height: '100px',
+                                }}>
+                                <span style={{fontWeight: '700'}}>
+                                    {color}
+                                </span>
+                            </div>
+                            <button type='button' onClick={() => onDelete(color)}>
+                                X
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
     );
-};
+}
+
 export default React.memo(ColorHistory);
